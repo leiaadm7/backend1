dc65cdf7a737f9d02061f3a4f1b7a11c793512b3
leiaadm7 <leia.donoso@inacapmail.cl>
2025-09-04 15:04:26 -0400

    parte 1 crud con django

---
+++ bienvenida/inventario/forms.py
@@ -0,0 +1,9 @@
+# inventario/forms.py
+from django import forms
+from .models import Producto
+
+class ProductoForm(forms.ModelForm):
+    class Meta:
+        model = Producto
+        fields = ['nombre', 'precio', 'descripcion', 'stock']
+
